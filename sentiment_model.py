8296de6c19e4e331da0ef0fe29a779a9de62e780
Shofi Sabilatus S <shofi10222145@sttcipasung.ac.id>
2026-01-04 11:59:02 +0700

    Update sentiment_model.py

--- sentiment_model.py
+++ sentiment_model.py
@@ -1,1 +1,45 @@
+import re
+import pickle
+import emoji
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+# ===============================
+# STEMMER
+# ===============================
+stemmer = StemmerFactory().create_stemmer()
+
+# ===============================
+# LOAD MODEL
+# ===============================
+model = pickle.load(open("model.pkl", "rb"))
+tfidf = pickle.load(open("tfidf.pkl", "rb"))
+
+# ===============================
+# PREPROCESSING
+# ===============================
+def preprocess(text):
+    if not isinstance(text, str):
+        return ""
+
+    text = emoji.replace_emoji(text, replace="")
+    text = text.lower()
+    text = re.sub(r"[^a-z\s]", " ", text)
+    text = re.sub(r"\s+", " ", text).strip()
+
+    return stemmer.stem(text)
+
+# ===============================
+# PREDIKSI SENTIMEN
+# ===============================
+def predict_sentiment(text):
+    clean_text = preprocess(text)
+
+    if clean_text == "":
+        return "netral"
+
+    vector = tfidf.transform([clean_text])
+    prediction = model.predict(vector)[0]
+
+    return prediction
+
 
